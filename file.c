b43301001a41356a9a59932004867281d6df4f5c
Henri Augusto <HenriAugusto@users.noreply.github.com>
2019-10-28 02:18:14 -0300

    Update file.c

--- file.c
+++ file.c
@@ -3,6 +3,8 @@
 //this is just a comment to test the fast forward method.
 //This was added after the Pull Request Was Created
 
+//this is going to be rebased
+
 #include "file.h"
 
 void function whatever(){return;}
